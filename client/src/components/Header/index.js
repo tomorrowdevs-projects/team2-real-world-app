e1caba2f46f2efc61d1e6a23ab86f7eccc6ea8ab
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2022-02-11 06:50:48 +0100

    [ADD] All the pages of the SPA close #79
    The pages (with placeholder content) are accessible both via the
    address bar and via temporary buttons.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
 const Header = () => {
-  return <h1>Header</h1>;
+  const navigate = useNavigate();
+  const pageLinks = [
+    { label: 'Log In', path: 'login' },
+    { label: 'Sign Up', path: 'signup' },
+  ];
+
+  return (
+    <>
+      <h1>Header</h1>
+      {pageLinks.map((link, index) => (
+        <Button key={index} onClick={() => navigate(`/${link.path}`)}>
+          {link.label}
+        </Button>
+      ))}
+    </>
+  );
 };
 
 export default Header;
--- client/src/components/NavApp/index.js
+++ client/src/components/NavApp/index.js
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
 const NavApp = () => {
-  return <h1>Navs</h1>;
+  const navigate = useNavigate();
+  const pageLinks = [
+    { label: 'Home', path: '' },
+    { label: 'Upload file', path: 'upload' },
+    { label: 'Queries', path: 'queries' },
+  ];
+  return (
+    <>
+      <hr />
+      {pageLinks.map((link, index) => (
+        <Button key={index} onClick={() => navigate(`/${link.path}`)}>
+          {link.label}
+        </Button>
+      ))}
+    </>
+  );
 };
 
 export default NavApp;
