14f55dec2911b8a9ff1def79aeed5369f8741ab5
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-12 20:16:29 +0100

    [FEAT] Dropdown menu component
    Provisional version useful as a placeholder for responsiveness.

---
+++ client/src/components/Menu/index.js
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router';
+import { Dropdown, NavDropdown } from 'react-bootstrap';
+import { ImExit } from 'react-icons/im';
+import { MdSettings } from 'react-icons/md';
+import userIcon from '../../assets/images/user-icon.png';
+
+const Menu = ({ currentUser, handleLogout, path }) => {
+  const navigate = useNavigate();
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant='primary' id='dropdown-basic'>
+        <img
+          src={userIcon}
+          alt='user profile photo'
+          style={{
+            height: '40px',
+            width: '40px',
+            borderRadius: '50%',
+            marginRight: '3px',
+          }}
+        />
+      </Dropdown.Toggle>
+      <Dropdown.Menu align='end'>
+        <Dropdown.Header></Dropdown.Header>
+        <Dropdown.Header></Dropdown.Header>
+        <NavDropdown.Divider />
+        <Dropdown.Item href='#/action-1' onClick={() => navigate(path)}>
+          <MdSettings className='me-1' /> Account
+        </Dropdown.Item>
+        <Dropdown.Item
+          href='#/action-3'
+          disabled={!currentUser}
+          onClick={handleLogout}
+        >
+          <ImExit className='me-1' />
+          Log Out
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default Menu;
