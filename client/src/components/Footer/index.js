72dd33e29ecb67731d56613d3329633396c285f8
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-13 18:54:44 +0100

    [FEAT] Improvements in the footer
    Added texts.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,25 @@
 import { Container } from 'react-bootstrap';
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillFacebook,
+  AiFillTwitterSquare,
+} from 'react-icons/ai';
 import './footer.scss';
 
 const Footer = () => {
   return (
-    <footer className='container-footer'>
+    <footer className='container-footer d-flex align-items-center'>
       <Container>
-        <h1>Footer</h1>
+        <div className='d-sm-flex justify-content-between text-center'>
+          <span>© 2021 Company, Inc. All rights reserved.</span>
+          <div className='social-icon'>
+            <AiFillGithub />
+            <AiFillLinkedin />
+            <AiFillFacebook />
+            <AiFillTwitterSquare />
+          </div>
+        </div>
       </Container>
     </footer>
   );
