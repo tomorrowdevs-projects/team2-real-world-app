6a02cdd643a954ac2db174fd4ae8f1bbb234297f
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-13 18:52:21 +0100

    [FEAT] Home page improvements
    Added texts and background.

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,8 +2,13 @@ import './home.scss';
 
 const Home = () => {
   return (
-    <div className='container-home'>
-      <h1>Home</h1>
+    <div className='container-home d-flex align-items-center'>
+      <main className='home-main'>
+        <h1>X-Comm App improves your business</h1>
+        <p>Try our services</p>
+        <p>Upload of large order lists</p>
+        <p>Consultation and statistics of the uploaded data</p>
+      </main>
     </div>
   );
 };
