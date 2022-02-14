2ae29e77dd799926b516c46dc4b12718d1f1124d
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-14 14:16:17 +0100

    [FEAT] Improvements in the upload section
    Added component containing presentation logic.

---
+++ client/src/pages/UploadFile/UploadFile.js
@@ -0,0 +1,50 @@
+import { Form, Button, ProgressBar } from 'react-bootstrap';
+import './upload-file-section.scss';
+
+const UploadFileSection = ({
+  progress,
+  handleSubmit,
+  handleInputFile,
+  cancelUpload,
+}) => {
+  return (
+    <main>
+      <section>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId='formFileLg'>
+            <Form.Label className='mt-2'>
+              <h1>Upload your file .csv</h1>
+            </Form.Label>
+            <Form.Control
+              type='file'
+              size='lg'
+              onChange={handleInputFile}
+              // accept='.csv'
+            />
+          </Form.Group>
+          <div
+            style={{
+              height: '2rem',
+            }}
+          >
+            {progress && (
+              <ProgressBar now={progress} label={`${progress}%`} animated />
+            )}
+          </div>
+          <div className='button-box'>
+            <Button variant='primary' type='submit'>
+              Upload
+            </Button>
+            {progress && progress < 100 && (
+              <Button variant='outline-primary' onClick={() => cancelUpload()}>
+                Cancel
+              </Button>
+            )}
+          </div>
+        </Form>
+      </section>
+    </main>
+  );
+};
+
+export default UploadFileSection;
