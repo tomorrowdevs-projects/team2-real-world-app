fcceef07f170e5f2b75c36e980441d4d59c671f1
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-14 15:08:07 +0100

    [FIX] Changes in the style of the Home component

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,14 +2,14 @@ import './home.scss';
 
 const Home = () => {
   return (
-    <div className='container-home d-flex align-items-center'>
-      <main className='home-main'>
+    <main className='container-home d-flex align-items-center'>
+      <section className='section-main'>
         <h1>X-Comm App improves your business</h1>
         <p>Try our services</p>
         <p>Upload of large order lists</p>
         <p>Consultation and statistics of the uploaded data</p>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 };
 
