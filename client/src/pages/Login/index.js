83914bff1db0f616d59df3770be386bb28765c0a
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-14 19:39:36 +0100

    [FEAT] Updated the Login component
    Added buttons for authentication with providers.

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,20 @@
+import { Card } from 'react-bootstrap';
 import './login.scss';
+import SignInButton from '../../components/SignInButtons';
 
 const Login = () => {
   return (
     <div className='container-login'>
-      <h1>Login</h1>
+      <Card className='pb-2'>
+        <Card.Header>
+          <h3 className='text-center'>Authenticate</h3>
+        </Card.Header>
+        <Card.Body>
+          <SignInButton provider='Google' />
+          <SignInButton provider='Facebook' />
+          <SignInButton provider='GitHub' />
+        </Card.Body>
+      </Card>
     </div>
   );
 };
