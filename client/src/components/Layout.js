d843b5f3a559e7f6e9e87a256e2539fe441bc2b3
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-15 15:32:10 +0100

    [FEAT] Update AlertMesssage component
    The component that displays messages to the user now receives properties
    from Context via reducer. Style improved.

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './Header';
+import AlertMessage from './AlertMessage/AlertMessage';
 import Footer from './Footer';
 
 const Layout = () => {
   return (
     <div className='container-main'>
       <Header />
+      <AlertMessage />
       <Container className='container-center'>
         <Outlet />
       </Container>
