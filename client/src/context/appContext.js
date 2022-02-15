edcfa8730ea35ad2541c7d7410c6365a7c41ec8e
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-15 12:30:42 +0100

    [FEAT] Application reducer set
    Using useReducer.
    First implementation of the state with the data relating to the alert to
    the user.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,9 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
+import reducer from './reducer';
 
 const AppContext = React.createContext();
 
+const defaultState = {
+  alert: {
+    show: false,
+    message: '',
+    variant: '',
+    dismissibile: true,
+    animation: '',
+  },
+};
+
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleAlert = (...value) => {
+    dispatch({ type: 'HANDLE_ALERT', payload: value });
+  };
+
+  return (
+    <AppContext.Provider value={{ ...state, handleAlert }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useAppContext = () => {
