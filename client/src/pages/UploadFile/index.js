e139cdd8291c4ffb3b36b633551080565b8971a3
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-15 17:25:44 +0100

    [FEAT] File upload logic
    The app allows you to choose a file that is sent, at the moment via
    localhost, manages the response from the server and messages to the
    user. There is a validation on the presence or absence of a file before
    uploading. At this stage it is possible to send all types of files,
    subsequently only .csv files.

--- client/src/pages/UploadFile/index.js
+++ client/src/pages/UploadFile/index.js
@@ -1,7 +1,56 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAppContext } from '../../context/appContext';
 import UploadFileSection from './UploadFile';
 
 const UploadFile = () => {
-  return <UploadFileSection />;
+  const [file, setFile] = useState(null);
+  const { handleAlert } = useAppContext();
+
+  const handleInputFile = event => {
+    setFile(event.target.files[0]);
+    handleAlert(false);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (!file) {
+      handleAlert(true, 'Please, choose a file first.', 'danger', true);
+      return;
+    }
+    const fileData = new FormData();
+    fileData.append('file', file);
+    handleAlert(false);
+
+    const option = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    axios
+      .post('//localhost:8000/upload', fileData, option)
+      .then(response => {
+        console.log('Success!', response, response.data);
+        handleAlert(
+          true,
+          `File ${response.data.filename.slice(14)} loaded successfully`,
+          'success',
+          false
+        );
+      })
+      .catch(error => {
+        console.log('Sorry, an error occurred.', error);
+        handleAlert(true, `Sorry... ${error}`, 'danger', true);
+      });
+  };
+  return (
+    <UploadFileSection
+      handleInputFile={handleInputFile}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default UploadFile;
