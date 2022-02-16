3bcbe4c46b95cbf27e7a2ec5be545ea44487ac84
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-16 13:37:29 +0100

    [FEAT] Import Firebase app and methods in Context
    The Firebase methods, all related to authentication management, are then
    passed to the Context Provider to make them available throughout the
    application.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,13 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useEffect } from 'react';
 import reducer from './reducer';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { appFirebase } from '../services/auth/firebase-config';
 
 const AppContext = React.createContext();
 
@@ -16,12 +24,25 @@ const defaultState = {
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
+  //Auth
+  const auth = getAuth(appFirebase);
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
   const handleAlert = (...value) => {
     dispatch({ type: 'HANDLE_ALERT', payload: value });
   };
 
   return (
-    <AppContext.Provider value={{ ...state, handleAlert }}>
+    <AppContext.Provider
+      value={{ ...state, signInWithGoogle, logout, handleAlert }}
+    >
       {children}
     </AppContext.Provider>
   );
