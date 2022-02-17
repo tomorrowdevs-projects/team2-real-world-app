bf6f305986514052110837baca8b06c7c1c1a9cb
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-17 20:17:23 +0100

    [FEAT] Dashboard page completed

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,9 +1,37 @@
+import { Card, ListGroup } from 'react-bootstrap';
+import { ImExit } from 'react-icons/im';
 import './dashboard.scss';
+import { useAppContext } from '../../context/appContext';
+import userIcon from '../../assets/images/user-icon.png';
+import { getUserData } from '../../assets/scripts/utils/dataManagement';
 
 const Dashboard = () => {
+  const { currentUser, logout } = useAppContext();
+
   return (
     <div className='container-dashboard'>
-      <h1>Dashboard</h1>
+      <Card>
+        <Card.Header className='d-flex justify-content-between'>
+          <h1>Dashboard</h1>
+          <img
+            src={getUserData(currentUser, 'photo', userIcon)}
+            alt='user profile'
+            className='user-photo'
+          />
+        </Card.Header>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            Name: {getUserData(currentUser, 'userName')}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Email: {getUserData(currentUser, 'email')}
+          </ListGroup.Item>
+          <ListGroup.Item className='dashboard-logout' onClick={logout}>
+            <ImExit className='me-1' />
+            Log Out
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
     </div>
   );
 };
