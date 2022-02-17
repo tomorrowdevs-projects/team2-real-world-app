38834fef57a23be2ecd519d19b487395b3fbfef1
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-17 20:16:09 +0100

    [FEAT] Component menu uses new function
    to decrease the risk of displaying corrupt or non-compliant data on the
    page.

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -4,28 +4,27 @@ import { ImExit } from 'react-icons/im';
 import { MdSettings } from 'react-icons/md';
 import './menu.scss';
 import userIcon from '../../assets/images/user-icon.png';
+import { getUserData } from '../../assets/scripts/utils/dataManagement';
 
 const Menu = ({ currentUser, handleLogout, path }) => {
   const navigate = useNavigate();
 
-  const getData = data => {
-    if (currentUser) {
-      return currentUser[data];
-    } else if (data !== 'photo') {
-      return '';
-    } else {
-      return userIcon;
-    }
-  };
-
   return (
     <Dropdown>
       <Dropdown.Toggle variant='outline-dark' id='dropdown-user'>
-        <img src={getData('photo')} alt='user profile' className='user-photo' />
+        <img
+          src={getUserData(currentUser, 'photo', userIcon)}
+          alt='user profile'
+          className='user-photo'
+        />
       </Dropdown.Toggle>
       <Dropdown.Menu align='end'>
-        <Dropdown.Header>{getData('userName')}</Dropdown.Header>
-        <Dropdown.Header>{getData('email')}</Dropdown.Header>
+        <Dropdown.Header>
+          {getUserData(currentUser, 'userName', userIcon)}
+        </Dropdown.Header>
+        <Dropdown.Header>
+          {getUserData(currentUser, 'email', userIcon)}
+        </Dropdown.Header>
         <NavDropdown.Divider />
         <Dropdown.Item
           disabled={currentUser === null}
