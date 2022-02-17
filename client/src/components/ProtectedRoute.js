daddf5f6e52a95200902a5ac583df973b5282014
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-02-17 11:45:18 +0100

    [FEAT] Protected routes send alerts to the user
    if authentication has not been performed.

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
 
 const ProtectedRoute = ({ user, redirectPath = '/login', children }) => {
+  const { handleAlert } = useAppContext();
+
+  useEffect(() => {
+    !user && handleAlert(true, 'Please, log in first.', 'info', false);
+  }, [user, handleAlert]);
+
   if (!user) {
     return <Navigate to={redirectPath} />;
   }
