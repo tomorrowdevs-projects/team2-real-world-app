cbc9f63d93f0f0bdbda8ef26b4198989d7c99391
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-02-19 19:25:20 +0100

    [FIX] updated test to interact with gin framework

--- server/pkg/api/upload_test.go
+++ server/pkg/api/upload_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,9 +15,15 @@ func TestUploadFile(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Set the header request
+	req.Header.Set("Content-Type", "application/json")
+
 	// Create a new recorder to record the response received by the /upload endpoint.
 	rr := httptest.NewRecorder()
-	UploadFile(rr, req)
+	ctx, _ := gin.CreateTestContext(rr)
+	ctx.Request = &http.Request{
+		Header: make(http.Header),
+	}
 
 	// Check if the response status code is 200 or 400.
 	if status := rr.Code; (status != http.StatusOK) && (status != http.StatusBadRequest) {
