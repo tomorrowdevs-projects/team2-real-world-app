d4af085c64cbf30e0ee7216504e80d88efbe2c5c
Isabel <isabel.lombardi96@gmail.com>
2022-02-22 21:43:13 +0100

    [FEAT] add db tags

--- server/pkg/model/entry.go
+++ server/pkg/model/entry.go
@@ -1,17 +1,17 @@
 package model
 
-import "gorm.io/gorm"
+//import "gorm.io/gorm"
 
 // NOTE: only for TESTING/POC PURPOSE (it reflects the exact structure of the CSV
 
 type Entry struct {
-	gorm.Model
-	OrderID   int     `csv:"orderID"`
-	ClientID  int     `csv:"clientID"`
-	Name      string  `csv:"name"`
-	Surname   string  `csv:"surname"`
-	ProductID int     `csv:"productID"`
-	Product   string  `csv:"product"`
-	Price     float64 `csv:"price"`
-	Date      string  `csv:"date"`
+	// multiple name tags
+	OrderID   int     `csv:"orderID"  db:"order_id"`
+	ClientID  int     `csv:"clientID" db:"client_id"`
+	Name      string  `csv:"name" db:"name"`
+	Surname   string  `csv:"surname" db:"surname"`
+	ProductID int     `csv:"productID"  db:"product_id"`
+	Product   string  `csv:"product"  db:"product_name"`
+	Price     float64 `csv:"price" db:"price"`
+	Date      string  `csv:"date" db:"order_date"`
 }
