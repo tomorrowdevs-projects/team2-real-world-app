469dcd23a5bb7dad375768c3583ef671adefac6e
Isabel <isabel.lombardi96@gmail.com>
2022-02-23 08:33:18 +0100

    [FEAT] perform error handle

--- server/pkg/file/file.go
+++ server/pkg/file/file.go
@@ -3,20 +3,20 @@ package file
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"github.com/gocarina/gocsv"
 	"os"
 	"path/filepath" // check file extension
 	helpers "team2-real-world-app/server/pkg/helpers"
-	"team2-real-world-app/server/pkg/model"
+	model "team2-real-world-app/server/pkg/model"
 )
 
-// InvalidFileExtensionError handle errors
 var (
 	ErrInvalidFileExtension = errors.New("the file extension is not .csv")
 	ErrInvalidFileSize      = errors.New("the file size exceeds the maximum allowed")
 )
 
-// struct Entry global var
+// TEMPORARY struct Entry global var
 var entries []model.Entry
 
 type ImportFile struct {
@@ -27,19 +27,15 @@ func NewImportFile() *ImportFile {
 	return &ImportFile{} // to get the address or a pointer variable
 }
 
-// SplitFile for local testing, waiting to understand type of passed file
+//
 func (importFile ImportFile) SplitFile(filePath string) error {
 
 	if !importFile.isCsvExtension(filePath) { // check file extension
 		return ErrInvalidFileExtension
 	}
 
-	// returns the FileInfo structure that describing file
-	fileInfo, err := os.Stat(filePath)
-	if err != nil {
-		return err
-	}
-	if !importFile.isTheRightSize(fileInfo) {
+	status, err := importFile.isTheRightSize(filePath)
+	if !status {
 		return ErrInvalidFileSize
 	}
 
@@ -54,13 +50,14 @@ func (importFile ImportFile) SplitFile(filePath string) error {
 
 	// TO DO
 	// split file in x chunk and pass it to io.Reader
-
 	fileReader := bufio.NewReader(file) // implements a buffered reader
 	err = importFile.populateStruct(fileReader)
 	if err != nil {
 		return err
 	}
-	//log.Println(entries)
+	fmt.Println("struct populated")
+
+	fmt.Println(entries)
 	return nil
 }
 
@@ -70,14 +67,20 @@ func (importFile ImportFile) isCsvExtension(filePath string) bool {
 }
 
 // check if the file size does not exceed the maximum allowed
-func (importFile ImportFile) isTheRightSize(info os.FileInfo) bool {
+func (importFile ImportFile) isTheRightSize(filePath string) (bool, error) {
 
 	maxSize := 10 // max size of the file
 
-	fileSize := info.Size()
+	// FileInfo structure that describing file
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return false, err
+	}
+
+	fileSize := fileInfo.Size()
 	fileGB := helpers.BytesToGB(fileSize)
 
-	return int64(fileGB) < int64(maxSize)
+	return int64(fileGB) < int64(maxSize), err
 }
 
 // save the file row into a struct
@@ -86,8 +89,8 @@ func (importFile ImportFile) populateStruct(fileReader *bufio.Reader) error {
 	err := gocsv.Unmarshal(fileReader, &entries)
 
 	if err != nil {
+		// handle returned errors
 		return err
 	}
-
-	return nil
+	return err
 }
