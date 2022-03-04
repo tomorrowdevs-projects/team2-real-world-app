9521d14e7b1f504d3a8b2660d3513961754db1e2
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-03-04 08:56:01 +0100

    [FIX] in the DataLabel component
    removes the date format conversion logic.

--- client/src/components/DateLabel/DateLabel.js
+++ client/src/components/DateLabel/DateLabel.js
@@ -1,14 +1,11 @@
 import './date-label.scss';
 
 const DateLabel = ({ dateText, dateFrom, dateTo }) => {
-  const convertDate = date =>
-    `${date.split('-')[2]}/${date.split('-')[1]}/${date.split('-')[0]}`;
   return (
     <>
       <p className='date-range-label mb-0 fs-4'>{dateText}</p>
       <p className='date-range-label'>
-        from <span>{convertDate(dateFrom)}</span> to{' '}
-        <span>{convertDate(dateTo)}</span>
+        from <span>{dateFrom}</span> to <span>{dateTo}</span>
       </p>
     </>
   );
