f046499567ba8ca1b0fd74b35464031c0584007b
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-03-04 08:50:53 +0100

    [FEAT] isInvalidDateRangeProp function
    with related tests.

--- client/src/assets/scripts/utils/date_utility/date_utility.js
+++ client/src/assets/scripts/utils/date_utility/date_utility.js
@@ -21,3 +21,20 @@ export const isValidDateRange = (dateInput1, dateInput2) => {
     Date.parse(dateInput1) <= Date.parse(dateInput2)
   );
 };
+
+/**
+ * Validate that the date format is correct and that the date range is invalid
+ * @param {string} dateInput1
+ * @param {string} dateInput2
+ * @returns {boolean}
+ */
+export const isInvalidDateRangeProp = (dateInput1, dateInput2) => {
+  if (!isValidDate(dateInput1) || !isValidDate(dateInput2)) {
+    return false;
+  } else if (isValidDateRange(dateInput1, dateInput2)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
