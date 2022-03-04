9429dec03db6014bd32303fa784614eb8e8c2c72
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-03-04 09:00:22 +0100

    [FEAT] in SearchResult component adds
    a conditional render based on the prop isInvalidDateRangeProp.

--- client/src/components/SearchResult/SearchResult.js
+++ client/src/components/SearchResult/SearchResult.js
@@ -3,6 +3,7 @@ import DateLabel from '../DateLabel/DateLabel';
 
 const SearchResult = ({
   select,
+  isInvalidDateRangeProp,
   responseLabel,
   dateText,
   startDate,
@@ -13,45 +14,48 @@ const SearchResult = ({
   numberOfClients,
   ordersAvg,
 }) => {
-  return (
-    <div className='container-result mt-4'>
-      <DateLabel dateText={dateText} dateFrom={startDate} dateTo={endDate} />
-      <Table striped bordered hover size='sm'>
-        {select ? (
-          <>
-            <thead>
-              <tr>
-                {responseLabel.map((item, index) => (
-                  <th key={index}>{item}</th>
-                ))}
-              </tr>
-            </thead>
+  if (isInvalidDateRangeProp) {
+    return <h4>Invalid date range. Data not available.</h4>;
+  } else
+    return (
+      <div className='container-result mt-4'>
+        <DateLabel dateText={dateText} dateFrom={startDate} dateTo={endDate} />
+        <Table striped bordered hover size='sm'>
+          {select ? (
+            <>
+              <thead>
+                <tr>
+                  {responseLabel.map((item, index) => (
+                    <th key={index}>{item}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{productName}</td>
+                  <td>{totalOrders}</td>
+                  <td>€ {revenue}</td>
+                </tr>
+              </tbody>
+            </>
+          ) : (
             <tbody>
               <tr>
-                <td>{productName}</td>
-                <td>{totalOrders}</td>
-                <td>€ {revenue}</td>
+                <td>
+                  <h5>{responseLabel}</h5>
+                </td>
+                <td>
+                  <h5 className='text-center'>
+                    {responseLabel === 'Customers' && numberOfClients}
+                    {responseLabel === 'Average' && ordersAvg}
+                  </h5>
+                </td>
               </tr>
             </tbody>
-          </>
-        ) : (
-          <tbody>
-            <tr>
-              <td>
-                <h5>{responseLabel}</h5>
-              </td>
-              <td>
-                <h5 className='text-center'>
-                  {responseLabel === 'Customers' && numberOfClients}
-                  {responseLabel === 'Average' && ordersAvg}
-                </h5>
-              </td>
-            </tr>
-          </tbody>
-        )}
-      </Table>
-    </div>
-  );
+          )}
+        </Table>
+      </div>
+    );
 };
 
 export default SearchResult;
