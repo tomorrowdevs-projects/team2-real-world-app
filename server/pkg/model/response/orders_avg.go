7a07d00c8ced929b114d6e75d49c26266e1a1681
Isabel <isabel.lombardi96@gmail.com>
2022-03-05 10:53:26 +0100

    [FEAT] create a response model struct of orders avg close #140

--- server/pkg/model/response/orders_avg.go
+++ server/pkg/model/response/orders_avg.go
@@ -1,7 +1,8 @@
 package response
 
+// OrdersAvg - model of the struct response used for the function orders AVG
 type OrdersAvg struct {
-	OrdersValue float64 `json:"orders_avg"`
-	StartDate   string  `json:"start_date"`
-	EndDate     string  `json:"end_date"`
+	OrdersValue float64 `db:"COUNT(id)"           json:"orders_avg"`
+	StartDate   string  `db:"MIN(date)"           json:"start_date"`
+	EndDate     string  `db:"MAX(date)"           json:"end_date"`
 }
