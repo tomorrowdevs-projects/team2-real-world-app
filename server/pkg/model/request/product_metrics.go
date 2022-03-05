f1264b181595002122a6c188f9718650dcc83f5d
Isabel <isabel.lombardi96@gmail.com>
2022-03-05 16:50:53 +0100

    [FEAT] create product metrics request model struct close #140

---
+++ server/pkg/model/request/product_metrics.go
@@ -0,0 +1,8 @@
+package request
+
+// ProductMetrics - model of the struct request used for the function productMetrics
+type ProductMetrics struct {
+	ProductID string
+	StartDate string
+	EndDate   string
+}
