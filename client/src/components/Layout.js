9cf8e50c1d6042f31c7a9684082c57e3ac9d4a60
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-03-05 19:29:27 +0100

    [FEAT] Add ProgressBarComponent to the app
    Adapted the following components and their style:
    Layout, AlertMessage, AlertMessageSearch

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,12 +3,16 @@ import { Container } from 'react-bootstrap';
 import Header from './Header';
 import AlertMessage from './AlertMessage/AlertMessage';
 import Footer from './Footer';
+import ProgressBarUpload from './ProgessBarUpload/ProgressBarUpload';
 
 const Layout = ({ dNone, children }) => {
   return (
     <div className='container-main'>
       <Header dNone={dNone} />
-      <AlertMessage />
+      <Container className='container-top'>
+        <AlertMessage />
+        <ProgressBarUpload />
+      </Container>
       <Container className='container-center'>
         {children ? children : <Outlet />}
       </Container>
