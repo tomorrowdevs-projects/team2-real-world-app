83cfe87382c6cc41a84170d0869d7d38995187e8
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-03-12 22:53:54 +0100

    [Fix] Monetary values (#164)
    * [FIX] getResponseData function
    
    which takes care of validating single properties of an array of objects,
    returning a default value in case of non-conforming or invalid data.
    Comments improved.
    
    * [FIX] Test getResponseData function
    
    Unit test for the new version of the getResponseData function.
    
    * [FIX] Search page props updated
    
    with the new new version of the getResponseData function.
    Improved comments.

--- client/src/assets/scripts/utils/dataManagement.js
+++ client/src/assets/scripts/utils/dataManagement.js
@@ -4,16 +4,15 @@ import { isValidDate } from './date_utility/date_utility';
  * User data management after authentication
  * Goal: not to create errors in case of corrupted data
  * If the photo is not required, the third parameter is optional
- * @param {Object} user
- * @param {string} data
- * @param {string} defaultData
+ * @param {Object} user user data
+ * @param {string} prop Properties of the customized object to use
+ * @param {string} defaultPhoto path of default image if the data is not available
  * @returns {string}
  */
-
-export const getUserData = (user, data, defaultPhoto) => {
-  return user && typeof user == 'object' && user[data]
-    ? user[data]
-    : data !== 'photo'
+export const getUserData = (user, prop, defaultPhoto) => {
+  return user && typeof user == 'object' && user[prop]
+    ? user[prop]
+    : prop !== 'photo'
     ? ''
     : defaultPhoto;
 };
@@ -21,11 +20,12 @@ export const getUserData = (user, data, defaultPhoto) => {
 /**
  * Response query data management
  * Goal: not to create errors in case of corrupted data
- * @param {any} prop
- * @param {string} type
- * @param {string} text
- * @param {string} date
- * @returns {any}
+ * @param {array of object} list response received
+ * @param {string} prop property of the response to use
+ * @param {string} type Typing of the expected data
+ * @param {string} text text to be returned if the data is not available
+ * @param {string} option At the moment it concerns dates or values followed by a comma and two numbers
+ * @returns {any} It depends on the input value
  */
 export const getResponseData = (list, prop, type, text, option = null) => {
   if (!list) {
@@ -37,8 +37,9 @@ export const getResponseData = (list, prop, type, text, option = null) => {
       ? list[0][prop]
       : text;
   } else if (option === 'float(2)') {
-    return list[0][prop] && typeof list[0][prop] === type
-      ? parseFloat(list[0][prop].toFixed(2))
+    return (list[0][prop] || list[0][prop] === 0) &&
+      typeof list[0][prop] === type
+      ? list[0][prop].toFixed(2)
       : text;
   } else {
     return list[0][prop] && typeof list[0][prop] === type
